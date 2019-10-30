dd8ad50e3c8e759a127cf9fc3f1d33cddf537151
Natalia Barthe <natibarthe29@hotmail.com>
2019-10-30 13:50:38 -0300

    routes with comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'participation', to: 'users#index'
+  # leer tudos os usuarios
+  get 'users', to: 'users#index'
+  # criar um novo usuario (2 requests)
+  get 'users/new', to: 'users#new'
+  post 'users', to: 'users#create'
 
+  # fazer o index a homepage
   root to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
