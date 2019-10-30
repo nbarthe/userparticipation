420689ccb8d744a87040580841bb76713bb3bc51
Natalia Barthe <natibarthe29@hotmail.com>
2019-10-30 15:11:34 -0300

    index view updated to display user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # leer tudos os usuarios
   get 'users', to: 'users#index'
-  # criar um novo usuario (2 requests)
+  # criar um novo usuario (2 requests):
+  # render the form new
   get 'users/new', to: 'users#new'
+  # enviar a info
   post 'users', to: 'users#create'
 
   # fazer o index a homepage
